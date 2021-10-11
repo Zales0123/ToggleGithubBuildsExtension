56fc764c5a4d238c4a111cd2bdd59a356c33d7a0
Mateusz Zalewski <zaleslaw@gmail.com>
2021-10-11 21:19:22 +0200

    Refactor button functions

--- addButton.js
+++ addButton.js
@@ -1,24 +1,25 @@
 function addToggleSuccessBuildButton() {
     let hideAllChecksButton = document.querySelector('.mergeability-details .branch-action-item:nth-child(2) div:nth-child(2) button');
 
-    let toggleSuccessBuildsButton = document.createElement('button');
-    toggleSuccessBuildsButton.setAttribute('aria-expanded', false);
-    toggleSuccessBuildsButton.setAttribute('class', 'btn-link float-right');
-    toggleSuccessBuildsButton.innerText = 'Hide success builds';
-    toggleSuccessBuildsButton.style = "margin-left: 1rem;";
-    toggleSuccessBuildsButton.addEventListener('click', () => {
-        document.querySelectorAll('.octicon-check.color-text-success').forEach(function(element) {
-            let actionItem = element.parentElement.parentElement;
-            let offsetParent = element.parentElement.offsetParent;
-            if (offsetParent == null) {
-                actionItem.setAttribute('style', "display: flex !important;");
-                toggleSuccessBuildsButton.innerText = 'Hide success builds';
-            } else {
-                actionItem.setAttribute('style', "display: none !important;");
-                toggleSuccessBuildsButton.innerText = 'Show success builds';
-            }
-        })
-    })
+    let toggleSuccessBuildsButton = document.createElement('div');
+    toggleSuccessBuildsButton.innerHTML = '<button class="btn-link float-right" style="margin-left: 1rem;">Hide success builds</button>'
+    toggleSuccessBuildsButton = toggleSuccessBuildsButton.firstChild;
+
+    toggleSuccessBuildsButton.addEventListener('click', toggleSuccessBuildsButtonClickCallback)
 
     hideAllChecksButton.parentNode.insertBefore(toggleSuccessBuildsButton, hideAllChecksButton);
+}
+
+function toggleSuccessBuildsButtonClickCallback(evt) {
+    document.querySelectorAll('.octicon-check.color-text-success').forEach(function(element) {
+        let actionItem = element.parentElement.parentElement;
+        let offsetParent = element.parentElement.offsetParent;
+        if (offsetParent == null) {
+            actionItem.setAttribute('style', "display: flex !important;");
+            evt.innerText = 'Hide success builds';
+        } else {
+            actionItem.setAttribute('style', "display: none !important;");
+            evt.innerText = 'Show success builds';
+        }
+    })
 }
